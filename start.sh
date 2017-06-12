65e375f57976d4ab1d66e50e8325215e156d1189
Lindsey Kitchell <lindseykitchell21@gmail.com>
2017-06-12 17:36:57 -0400

    Create start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #mainly to debug locally
-if [ -z $SCA_WORKFLOW_DIR ]; then export SCA_WORKFLOW_DIR=`pwd`; fi
-if [ -z $SCA_TASK_DIR ]; then export SCA_TASK_DIR=`pwd`; fi
-if [ -z $SCA_SERVICE_DIR ]; then export SCA_SERVICE_DIR=`pwd`; fi
+if [ -z $WORKFLOW_DIR ]; then export WORKFLOW_DIR=`pwd`; fi
+if [ -z $TASK_DIR ]; then export TASK_DIR=`pwd`; fi
+if [ -z $SERVICE_DIR ]; then export SERVICE_DIR=`pwd`; fi
 
 #clean up previous job (just in case)
 rm -f finished
-jobid=`qsub $SCA_SERVICE_DIR/submit.pbs`
+jobid=`qsub $SERVICE_DIR/submit.pbs`
 echo $jobid > jobid
 
