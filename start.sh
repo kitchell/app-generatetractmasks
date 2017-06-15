5730e40d48c506151f251d9c78115c9c4b3344f9
Lindsey Kitchell <lindseykitchell21@gmail.com>
2017-06-15 13:56:58 -0400

    Create start.sh

--- start.sh
+++ start.sh
@@ -8,8 +8,6 @@ if [ -z $SERVICE_DIR ]; then export SERVICE_DIR=`pwd`; fi
 rm -f finished
 
 if [ $ENV == "IUHPC" ]; then
-	
-	
 	#jobid=`qsub $SERVICE_DIR/submit.pbs`
 	jobid=`qsub -q preempt $SERVICE_DIR/submit.pbs`
 	echo $jobid > jobid
