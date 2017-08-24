1a83c9b56579f3c8267901a8f7c40098cba5ae72
Lindsey Kitchell <lindseykitchell21@gmail.com>
2017-08-24 12:54:19 -0400

    Update main.m

--- main.m
+++ main.m
@@ -15,13 +15,13 @@ case 'IUHPC'
 	addpath(genpath('/N/u/hayashis/BigRed2/git/jsonlab'))
 	addpath(genpath('/N/u/kitchell/Karst/Applications/iso2mesh'))
 	addpath(genpath('/N/u/hayashis/BigRed2/git/vistasoft'))
-	addpath(genpath('/N/u/hayashis/BigRed2/git/afq'))
+	addpath(genpath('/N/u/hayashis/BigRed2/git/afq-master'))
 case 'VM'
 	disp('loading paths (VM)')
 	addpath(genpath('/usr/local/jsonlab'))
 	addpath(genpath('/usr/local/iso2mesh'))
 	addpath(genpath('/usr/local/vistasoft'))
-	addpath(genpath('/usr/local/afq'))
+	addpath(genpath('/usr/local/afq-master'))
 end
 
 % load config.json
