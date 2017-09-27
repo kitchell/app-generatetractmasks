c90e2b3dbb435afea8d36af5784709759a280e75
Lindsey Kitchell <lindseykitchell21@gmail.com>
2017-09-27 13:37:21 -0400

    Update main.m

--- main.m
+++ main.m
@@ -40,6 +40,7 @@ if config.clean_afq
     fg_classified = AFQ_clean(fg_classified);
 end
 
+eval(sprintf('!NUMFILES = $d', length(fg_classified)));
 
 thresholdPercent = 20;
 %islandFlag       = false;
